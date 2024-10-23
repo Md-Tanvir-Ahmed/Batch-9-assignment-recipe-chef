2342fe5baacadf6ef4f53155f0b4ae678b5b8947
Mohammad Tanvir Ahmed Abir <abir64072@gmail.com>
2024-10-23 17:35:32 +0600

    added cards upper portion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import Banner from "./components/Header/Banner";
 import Navbar from "./components/Header/Navbar";
+import RecipeSection from "./components/Header/RecipeSection";
+import Recipes from "./components/RecipeCards/Recipes";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 
 const App = () => {
   return (
-    <div>
+    <div className="md:w-11/12 mx-auto">
       {/* Header section  */}
       {/* Navbar */}
       <Navbar></Navbar>
 
       {/* Banner section */}
       <Banner></Banner>
+
+
+      {/* Our recipes section */}
+      <RecipeSection></RecipeSection>
+
+      {/* Recipe card section */}
+
+      <section className="flex flex-col md:flex-row gap-6">
+        {/* Cards section */}
+        <Recipes></Recipes>
+
+
+        {/* Sidebar */}
+        <Sidebar></Sidebar>
+
+
+      </section>
     </div>
   );
 };
---
+++ src/components/Header/RecipeSection.jsx
@@ -0,0 +1,15 @@
+
+
+const RecipeSection = () => {
+    return (
+        <div className="w-2/3 mx-auto mt-8">
+            <h2 className="text-4xl font-bold text-center">Our Recipies</h2>
+            <p className="text-center mt-3">Discover a world of flavors in our recipes section. From quick, healthy 
+                meals to indulgent gourmet dishes, each recipe is crafted with fresh ingredients and easy-to-follow 
+                steps,
+                 bringing the joy of cooking right to your kitchen.</p>
+        </div>
+    );
+};
+
+export default RecipeSection;
